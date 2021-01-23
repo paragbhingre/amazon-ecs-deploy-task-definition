d887dd7e75bf22decd2e167675323ce91263b838
Parag Bhingre <pbhingre@gmail.com>
2021-01-22 16:11:44 -0800

    added debug statement

--- index.js
+++ index.js
@@ -224,6 +224,7 @@ async function run() {
     }
 
     const forceNewDeployInput = core.getInput('force-new-deployment', { required: false });
+    core.debug("forceNewDeployInput --- " + typeof forceNewDeployInput);
     const forceNewDeployment = forceNewDeployInput != undefined && (forceNewDeployInput.toLowerCase === 'true' || forceNewDeployInput);
     
     // Register the task definition
