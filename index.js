d82524573e73497637ba2188a8860a51ad8db263
Parag Bhingre <pbhingre@amazon.com>
2021-03-23 13:07:40 -0700

    add debug

--- index.js
+++ index.js
@@ -146,6 +146,7 @@ function maintainAppMeshConfiguration(taskDef) {
   }
   core.debug('teaskDef Response -- ' + JSON.stringify(taskDef));
   core.debug('teaskDef Response -- ' + JSON.stringify(taskDef.proxyConfiguration));
+  core.debug('teaskDef Response -- ' + JSON.stringify(taskDef.proxyConfiguration));
   return taskDef;
 }
 
